68ad241fedf18df7a1e13200bf4dfbe7963b7914
jupyter <jupyter@notebooks.hpedev.io>
2020-06-02 22:12:17 +0200

    Add 2 scripts to manage users

---
+++ adduser.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+clear
+#ASK FOR USER TYPE TESTER CHALLENGER OR STUDENT
+typename=""
+while [ "$typename" = "" ]
+  do 
+	echo "Choose between student, challenger, or tester please"
+	read typename
+	if  [ "$typename" != "student" ] && [ "$typename" != "challenger" ] && [ "$typename" != "tester" ]; then
+		echo "invalid entry $typename"
+		typename=""
+	fi	
+  done
+
+echo type is  $typename
+
+#ASK FOR RANGE: Starting number and ending number separated by a space
+startramge=""
+endrange=""
+while [ "$startrange" = "" -a "$endrange" = "" ]
+  do
+	echo "Please provide range for user: starting and ending separated by a space:"
+	read startrange endrange
+  done
+echo range from $startrange to  $endrange
+
+echo this script will create usernames from $typename$startrange to $typename$endrange
+
+for i in $(seq $startrange $endrange); do username="$typename$i"; adduser $username --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password -ingroup jupyter; echo $username:hpeDEV2020 | chpasswd ; done
+
+#for i in $(cat devtalkuser.txt); do adduser $i --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password -ingroup jupyter; echo $i:dev2020 | chpasswd ; done
+
