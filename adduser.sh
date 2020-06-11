740387977bf7a247a6ea9e2ce2fdcf53542ae187
jupyter <jupyter@notebooks.hpedev.io>
2020-06-11 19:01:49 +0200

    adduser.sh modification & renme of deluser.sh
    -jupyter user creation
    -test on range integers
    -removal last line

--- adduser.sh
+++ adduser.sh
@@ -15,10 +15,10 @@ while [ "$typename" = "" ]
 echo type is  $typename
 
 #ASK FOR RANGE: Starting number and ending number separated by a space
-startramge=""
-endrange=""
-while [ "$startrange" = "" -a "$endrange" = "" ]
-  do
+startrange=0
+endrange=0
+while [[ $startrange -eq 0 || $endrange -eq 0 || $startrange -gt $endrange ]]
+do
 	echo "Please provide range for user: starting and ending separated by a space:"
 	read startrange endrange
   done
@@ -26,7 +26,13 @@ echo range from $startrange to  $endrange
 
 echo this script will create usernames from $typename$startrange to $typename$endrange
 
-for i in $(seq $startrange $endrange); do username="$typename$i"; adduser $username --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password -ingroup jupyter; echo $username:hpeDEV2020 | chpasswd ; done
+AdminToken="a1265a6032b544d890f8a69a4a9fd2c2"
 
-#for i in $(cat devtalkuser.txt); do adduser $i --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password -ingroup jupyter; echo $i:dev2020 | chpasswd ; done
-
+for i in $(seq $startrange $endrange) 
+do 
+	username="$typename$i"  
+	adduser $username --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password -ingroup jupyter
+	echo $username:hpeDEV2020 | chpasswd 
+	curl -X POST --silent -k -H "Authorization: token $AdminToken" https://16.31.85.200:8000/hub/api/users/$username | jq
+	echo $username created and Jupyter user
+done
