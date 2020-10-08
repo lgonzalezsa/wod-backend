c9e7105f6f0d56214aff6254b047a7d86027882c
jupyter <jupyter@notebooks.hpedev.io>
2020-10-08 18:29:35 +0000

    Improve messages and secure removal of home directories

--- cleanup.sh
+++ cleanup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# ggggggggggg
+#
 clear
 #ASK FOR USER TYPE TESTER CHALLENGER OR STUDENT
 typename=""
@@ -10,24 +10,31 @@ while [ "$typename" = "" ]
 	if  [ "$typename" != "student" ] && [ "$typename" != "challenger" ] && [ "$typename" != "tester" ]; then
 		typename=""
 	fi	
-  done
+done
 
 echo type is  $typename
 
 #ASK FOR RANGE: Starting number and ending number separated by a space
 startrange=0
 endrange=0
+
 # while [[ $endrange -eq 0 ]]
 while [[ $startrange -eq 0 || $endrange -eq 0 || $startrange -gt $endrange ]]
 do
 	echo "Please provide range for user: starting and ending separated by a space:"
 	read startrange endrange
-  done
+done
+
 echo range from $startrange to  $endrange
+echo this script will cleanup student folders  from $typename$startrange to $typename$endrange
 
-echo this script will empty home folders of  usernames from $typename$startrange to $typename$endrange
-
-for i in $(seq $startrange $endrange); do username="$typename$i"; sudo rm -rf /home/$username/*; done
+for i in $(seq $startrange $endrange); do 
+	username="$typename$i"
+	if [ _"$username" != _"" ]; then
+		rm -rf /home/$username/*
+	fi
+done
 
 #for i in $(cat devtalkuser.txt); do adduser $i --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password -ingroup jupyter; echo $i:dev2020 | chpasswd ; done
+
 
