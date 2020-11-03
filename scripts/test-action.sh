9988136d2aee013d688c5f6d25d75eb7398c6b4d
jupyter <jupyter@notebooks.hpedev.io>
2020-11-03 14:00:30 +0000

    Fixes for FW and postfix
    - Activate ufw
    - Adds incoming ssh and postfix support in ufw
    - Fix main.cf setup with myhostname/mydestinatio
    - Generate the test-action.sh script

--- scripts/test-action.sh
+++ scripts/test-action.sh
@@ -14,24 +14,12 @@ if [ $ACTION != "CREATE" ]  && [ $ACTION != "CLEANUP" ]  && [ $ACTION != "RESET"
 	echo "A correct ACTION is mandatory"
 	exit -1
 fi
-#Jupyterhub API variable depending on site
-if [ _"`hostname -s`" = _"jupyterhub2" ]; then
-	PORT=10025
-elif [ _"`hostname -s`" = _"jupyterhub" ]; then
-	PORT=10025
-elif [ _"`hostname -s`" = _"jupyterhub3" ]; then
-	PORT=10026
-else
-	echo "This machine is not a jupyterhub machine"
-	exit -1
-fi
-
 (
 	echo "MAIL FROM: hpedev.hackshack@hpe.com"
 	echo "RCPT TO: jupyter@`hostname`"
 	echo "DATA"
-	echo "Subject: $ACTION 2 4" 
+	echo "Subject: $ACTION 396 4" 
 	echo " "
-	echo "WKSHP-HPECP-API"
+	echo "WKSHP-API101"
 	echo "."
-) | telnet localhost $PORT
+) | telnet localhost 10026
