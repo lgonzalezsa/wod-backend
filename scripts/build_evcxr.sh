3b120e07d9bb583667a58d44494cd0d258b4b9cf
jupyter <jupyter@notebooks.hpedev.io>
2020-11-20 19:26:15 +0000

    Revert to using packaged rust
    - Install back again rust and cargo
    - add in repository the rust kernel (it needs to be compiled with a more
    recent rust manually)
    - add a script to perform that build

---
+++ scripts/build_evcxr.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+v=`rustc --version | awk '{ print $2 }' | cut -d. -f2`
+if [ _"$v"  = _"" || $v -le 43 ]; then
+	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs > /tmp/rust-init-$$.sh
+	chmod 755 /tmp/rust-init-$$.sh
+	/tmp/rust-init-$$.sh -y
+	. $HOME/.profile && cargo install cargo-script && cargo install evcxr_jupyter --no-default-features
+	cp ~/.cargo/bin/evcxr_jupyter $HOME/jupyter-procmail/
+	/tmp/rust-init-$$.sh self uninstall
+	rm -f /tmp/rust-init-$$.sh
+fi
