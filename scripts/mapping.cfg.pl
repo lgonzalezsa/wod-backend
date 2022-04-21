132f98de0368629b83d8f018109e704dffa7c50f
jupyter <jupyter@notebooks.hpedev.io>
2022-04-21 10:55:36 +0200

    First work on a rebranding script

---
+++ scripts/mapping.cfg.pl
@@ -0,0 +1,37 @@
+#!/usr/bin/perl -cw
+#
+# You should check the syntax of this file before using it in an auto-install.
+# You can do this with 'perl -cw mapping.cfg.pl' or by executing this file
+# (note the '#!/usr/bin/perl -cw' on the first line).
+
+our $maptable = {
+    'HPE DEV Team' => '{{ BRANDING }} Community Team',
+    'HPE DEV team' => '{{ BRANDING }} Community Team',
+    'collaborate with HPE DEV' => 'collaborate with {{ BRANDING }} Community Team',
+    'HPE DEV Workshops-on-Demand' => '{{ BRANDINGWOD }} Workshops-on-Demand',
+    'HPE DEV workshop-on-demand' => '{{ BRANDINGWOD }} Workshops-on-Demand',
+    'HPE DEV Community' => '{{ BRANDING }} Community',
+    'HPE DEV GitHub' => '{{ BRANDING }} Community GitHub',
+    'HPE DEV background' => '{{ BRANDING }} Community background',
+    'HPE DEV portal' => '{{ BRANDING }} Portal',
+    'HPE DEV Logo' => 'Workshops-on-Demand Logo',
+    'HPE DEV logo' => 'Workshops-on-Demand Logo',
+    'HPE DEV Munch-and-Learn' => '{{ BRANDING }} Munch-and-Learn',
+    'HPE DEV Slack Workspace' => '{{ BRANDING }} Community Slack Workspace',
+    'HPE DEV blog' => '{{ BRANDING }} Community Blog',
+    'HackShack workshops' => '{{ BRANDINGWOD }} Workshops-on-Demand',
+    'HackShack Workshops' => '{{ BRANDINGWOD }} Workshops-on-Demand',
+    'HackShack Workshop' => '{{ BRANDINGWOD }} Workshop-on-Demand',
+    'HackShack Labs and Open labs' => '{{ BRANDINGWOD }} Workshops-on-Demand',
+    'Python 101 HackShack' => '[Python 101 {{ BRANDINGWOD }} Workshop-on-Demand](https://developer.hpe.com/hackshack/workshop/15)',
+    'Ansible101 HPE DEV Worshop-on-Demand' => '[Ansible 101 {{ BRANDINGWOD }} Workshop-on-Demand](https://developer.hpe.com/hackshack/workshop/31)',
+    'HackShack' => 'Workshop-on-Demand',
+    'HPE DEV Hack Shack team' => '{{ BRANDING }} Community Team',
+    'HPE DEV Hack Shack Workshops-on-Demand' => '{{ BRANDINGWOD }} Workshops-on-Demand',
+    'HPE DEV Hack Shack CSI Workshop' => '[{{ BRANDINGWOD }} CSI Workshop-on-Demand](https://developer.hpe.com/hackshack/workshop/2)',
+    'Hack Shack tenant' => '{{ BRANDING }} Greenlake Hack Shack tenant',
+    'Hack Shack Workshops ' => '{{ BRANDINGWOD }} Workshops-on-Demand',
+    'HPE DEV Hack Shack technical workshops' => '{{ BRANDINGWOD }} Workshops-on-Demand',
+    'HPE DEV Hack Shack Workshops-on-Demand' => '{{ BRANDINGWOD }} Workshops-on-Demand',
+    'Pictures/hpe-dev-logo.png' => 'Pictures/wod-logo.png',
+};
---
+++ scripts/rebranding.pl
@@ -0,0 +1,56 @@
+#!/usr/bin/perl -w
+
+use strict;
+use Data::Dumper;
+use File::Find;
+
+our $maptable;
+eval { require "$ENV{'HOME'}/jupyter-procmail/scripts/mapping.cfg.pl"; };
+if ($@) {
+    print("Config file not found: mapping.cfg.pl\n");
+    exit(-1);
+}
+print("Hash is:\n");
+print(Dumper($maptable));
+
+my %maptable = %$maptable;
+
+my $dirname = $ARGV[0];
+
+if ((not defined $dirname) || (! -d $dirname )) {
+    print("Directory not found: $dirname\n") if (defined $dirname);
+    print("Syntax: rebranding.pl dirname\n");
+    exit(-1);
+}
+
+# Recurse from directory passed in parameter
+finddepth({ wanted => \&recurse1, no_chdir => 1},$dirname);
+
+sub recurse1 {
+
+   my $filename = $File::Find::name;
+
+   return if (-d $filename);
+   return if ($filename =~ /\/.git\//);
+   print("Handling $filename\n");
+   # Get all content into $text
+   my $sep = $/;
+   my $text = "";
+   $/ = undef;
+   if (open(FILE, "$filename")) {
+      $text = <FILE>;
+      close(FILE);
+   } else {
+      print "could not open $filename: $!";
+      return;
+   }
+   $/ = $sep;
+   # subsitute using external hash  
+   foreach my $key (keys %maptable) {
+      #print "Substitue string **$key** by **$maptable{$key}**\n";
+      $text =~ s/$key/$maptable{$key}/g;
+   }
+   open(FILE, "> $filename") || (print "could not write into $filename: $!" && return);
+   print FILE $text;
+   close(FILE);
+}
