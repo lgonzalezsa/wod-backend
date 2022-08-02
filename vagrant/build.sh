b343fe274fff4a1c135e3e2271278abb9e65f95c
Bruno Cornec <bruno@flossita.org>
2022-08-02 11:10:01 +0200

    Single Vagrantfile for the 2 supported distributions

--- vagrant/build.sh
+++ vagrant/build.sh
@@ -3,8 +3,9 @@
 set -e
 # Automate Wod server creation
 # By default work with CentOS 7 - 
+#default=ubuntu-20.04
+default=centos-7
 # Change the link to the Vagrantfile to point to an Ubuntu one
-ln -sf Vagrantfile.centos-7 Vagrantfile
-vagrant halt
-vagrant up
-vagrant ssh -c "sudo /vagrant/install.sh"
+vagrant halt $default
+vagrant up $default
+vagrant ssh  $default -c "sudo /vagrant/install.sh"
